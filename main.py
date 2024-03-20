f7d3a6fe5aeb6773ba1afdfd3d08ee016f3a565f
ktkravi <139673312+ktkravi@users.noreply.github.com>
2024-03-20 11:28:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,23 @@
 import streamlit as st
 import cv2
-import time
+import numpy as np
+import requests
 
-st.title('Using Mobile Camera with Streamlit')
-frame_window = st.image([])
-take_picture_button = st.button('Take Picture')
+st.title( 'Mobile Camera Preview in Streamlit' )
+frame_window = st.image( [] )
+take_picture_button = st.button( 'Take Picture' )
 
-# Add a 2-minute delay before attempting to open the video stream
-st.write("Checking connection...")
-time.sleep(120)
+while True:
+    # Request the image from the server
+    response = requests.get(url="http://<network_ip_address>:<port>/photo.jpg")
+    imgNp = np.array(bytearray(response.content), dtype=np.uint8)
+    frame = cv2.imdecode(imgNp, cv2.IMREAD_UNCHANGED )
+    # As OpenCV decodes images in BGR format, we'd convert it to the RGB format
+    frame = cv2.cvtColor( frame , cv2.COLOR_BGR2RGB )
 
-# Initialize video capture
-vid = cv2.VideoCapture('rtsp://192.168.141.166:8080/h264.sdp')
-
-# Check if the camera is opened successfully
-if not vid.isOpened():
-    st.error("Error: Unable to open camera stream.")
-    st.stop()
-
-# Loop until user takes a picture
-while not take_picture_button:
-    # Read frame from video stream
-    got_frame, frame = vid.read()
-    
-    if not got_frame:
-        st.warning("Warning: Unable to read frame from camera stream.")
-        break
-    
-    # Convert frame to RGB
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    
-    # Update displayed frame
     frame_window.image(frame)
-    
-    # Introduce delay to control update rate
-    time.sleep(0.1)
 
-# Release video capture resources
-vid.release()
+    if take_picture_button:
+        # Pass the frame to a model
+        # And show the output here...
+        break
