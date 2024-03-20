42f6d1710bcca12e392b75f24833d6eaf1a07a9d
ktkravi <139673312+ktkravi@users.noreply.github.com>
2024-03-20 11:29:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ take_picture_button = st.button( 'Take Picture' )
 
 while True:
     # Request the image from the server
-    response = requests.get(url="http://<network_ip_address>:<port>/photo.jpg")
+    response = requests.get(url="http://192.168.141.166:8080//photo.jpg")
     imgNp = np.array(bytearray(response.content), dtype=np.uint8)
     frame = cv2.imdecode(imgNp, cv2.IMREAD_UNCHANGED )
     # As OpenCV decodes images in BGR format, we'd convert it to the RGB format
